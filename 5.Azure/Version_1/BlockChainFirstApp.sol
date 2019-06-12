1104cd119c913a002e38e15a98bba9aea531da27
Thirumalaivasan S <THIRUMALAIVASAN9245@GMAIL.COM>
2019-06-12 17:43:47 +0100

    Add files via upload

---
+++ 5.Azure/Version_1/BlockChainFirstApp.sol
@@ -0,0 +1,108 @@
+pragma solidity >=0.4.25 <0.6.0;
+
+contract BlockChainFirstApp
+{
+     //Set of States
+    enum StateType { PolicyRequest, PolicyRespondInValid, PolicyRespondValid, SubmitBillDetailsRequest, SubmitBillDetailsResponse, RejectClaimRequest, ApprovedClaimRequest, HealthDBApprovalRequest }
+
+    //List of properties
+    StateType public  State;
+    address public  Hospital;
+    address public  Insurer;
+	address public  HealthDb;
+
+    string public RequestMessage;
+	string public Description;
+    string public ResponseMessage;	
+
+    // constructor function
+    constructor(string memory message) public
+    {
+        Insurer = msg.sender;
+        RequestMessage = message;
+        State = StateType.PolicyRequest;
+		Description = "Verify Policy Number - Request";
+    }
+
+    // call this function to verify policy request
+    function VerifyPolicyStatusRequest(string memory requestMessage) public
+    {
+        if (Hospital != msg.sender)
+        {
+            revert();
+        }
+
+        RequestMessage = requestMessage;
+        State = StateType.PolicyRequest;
+		Description = "Verify Policy Number - Request";
+    }
+
+    // call this function to return policy status 
+    function VerifyPolicyStatusResponseValid() public
+    {
+        Insurer = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = "Valid Policy Number";
+        State = StateType.SubmitBillDetailsRequest;
+		Description = "Verify Policy Number - Response";
+    }
+	
+	// call this function to return policy status 
+    function VerifyPolicyStatusResponseInValid() public
+    {
+        Insurer = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = "InValid Policy Number";
+        State = StateType.PolicyRespondInValid;
+		Description = "Verify Policy Number - Response";
+    }
+	
+	// call this function to return policy status 
+    function SubmitBillDetailsRequest(string memory treatmentType, string memory billNumber,string memory billAmount, string memory description) public
+    {
+        Insurer = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = treatmentType;
+		ResponseMessage = billNumber;
+		ResponseMessage = billAmount;
+		ResponseMessage = description;
+        State = StateType.SubmitBillDetailsResponse;
+		Description = "SubmitBillDetails - Request";
+    }
+	
+	// call this function to return policy status 
+    function RejectClaimRequest() public
+    {
+        Insurer = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = "Claim Rejacted";
+        State = StateType.RejectClaimRequest;
+		Description = "Claim Rejacted - Response";
+    }
+	
+	// call this function to return policy status 
+    function ApprovedClaimRequest() public
+    {
+        Insurer = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = "Claim Approved";
+        State = StateType.ApprovedClaimRequest;
+		Description = "Claim Approved - Response";
+    }
+	
+	// call this function to return policy status 
+    function HealthDBApprovalRequest() public
+    {
+        Insurer = msg.sender;
+
+        // call ContractUpdated() to record this action
+        ResponseMessage = "Verify Approval HealthDb - Request";
+        State = StateType.HealthDBApprovalRequest;
+		Description = "Approval HealthDB - Request";
+    }
+}
